0f239ce32be598f43dd2ef5a61ae8c6d8470e3a5
romanich141 <romaovcharenko18011993@gmail.com>
2019-09-02 17:31:50 +0300

    Add promise chain

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,29 @@
 import React from "react";
+import Header from "./Header/Header";
 import Filters from "./Filters/Filters";
 import MoviesList from "./Movies/MoviesList";
 
 export default class App extends React.Component {
   render() {
     return (
-      <div className="container">
-        <div className="row mt-4">
-          <div className="col-4">
-            <div className="card" style={{ width: "100%" }}>
-              <div className="card-body">
-                <h3>Фильтры:</h3>
-                <Filters />
+      <>
+        <Header />
+        <div className="container">
+          <div className="row mt-4">
+            <div className="col-4">
+              <div className="card" style={{ width: "100%" }}>
+                <div className="card-body">
+                  <h3>Фильтры:</h3>
+                  <Filters />
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-8">
-            <MoviesList />
+            <div className="col-8">
+              <MoviesList />
+            </div>
           </div>
         </div>
-      </div>
+      </>
     );
   }
 }
